2afddf23846e77cde072ed66f4a273e8952e4141
DaviRamalhow <daviramalho3011@gmail.com>
2022-08-08 16:29:03 -0300

    No-challenge edits

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,6 @@
     <br>
     <p v-html="linkHtml"></p>
 
-    <br>
-    <br>
-    <p>Usando VueJS - {{ nome }} ({{ idade }})</p>
-    <p>{{ mutiply() }}</p>
-    <p>{{ random() }}</p>
-    <br>
-    <img height="400" v-bind:src="imagem">
   </div>
 </template>
 <!--================ END TEMPLATE ================-->
@@ -34,24 +27,13 @@ const title = ref("Usando VuesJS 3");
 const dia = ref("dia 30");
 const link = ref("http://google.com.br");
 const linkHtml = ref('<a v-bind:href="link">google</a>');
-const nome = ref("Davi");
-const idade = ref("17");
-const imagem = ref("https://i.pinimg.com/originals/d5/d4/3a/d5d43a7dceef16619122735e7800ca08.jpg");
+
 
 const saudacao = () => {
   title.value = "Boa semana";
   return title.value;
 }
 
-const mutiply = () => {
-  let idade3 = idade.value * 3;
-  return idade3;
-}
-
-const random = () => {
-  let x = Math.random() * (0, 1);
-  return x;
-}
 </script>
 
 <!--================ STYLE ================-->
