3e4d2065d30f1c1dd430f7ddac1ba9165255f3e2
DaviRamalhow <daviramalho3011@gmail.com>
2022-08-09 10:54:59 -0300

    before

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,13 @@
 
   <div id="app">
 
-    <p>{{ contador }}</p>
-    <button @click="somar(3)">Somar 1</button>
-    <p @mousemove="mouseMove">Mouse:{{ x }} e {{ y }}
-      <span @mousemove.stop="mousemove">PARAR AQUI!!!!!!</span>
-    </p>
-    <hr>
-    <input type="text" @keyup.enter="exibirAlerta">
-    <br>
-    <br>
-
-    <button @click="exibirAlerta">Exibir alerta</button>
-    <input type="text" @keydown.enter="data = $event.target.value">
-    <p>{{ data }}</p>
+    <p>{{ titulo }}</p>
+    <input type="" v-model="titulo">
 
   </div>
 </template>
 <!--================ END TEMPLATE ================-->
+
 
 
 <!--================ SCRIPT ================-->
@@ -26,27 +16,10 @@
 import { ref } from "vue";
 //import { data } from "./utils";
 
-const contador = ref(0);
-const x = ref(0);
-const y = ref(0);
+const titulo = ref("Usando VueJS!");
 
-const mouseMove = (event) => {
-  x.value = event.clientX;
-  y.value = event.clientY;
-  console.log(event.clientX, event.clientY);
-}
-
-const somar = (X, ev) => {
-
-  console.log(x, ev);
-  contador.value = contador.value + X;
-}
-const exibirAlerta = () => {
-  alert("estou te alertando");
-}
-
-const data = ref(0);
 </script>
+
 
 
 <!--================ STYLE ================-->
