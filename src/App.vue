8f852514ad062bf98329a3f74ef086c329921b3b
DaviRamalhow <daviramalho3011@gmail.com>
2022-08-10 10:22:54 -0300

    done

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,29 @@
     <input type="text" v-model="cor">
     <input type="text" v-model="largura">
     <input type="text" v-model="altura">
+    <br>
+    <button @click="iniciarEfeito">iniciar Efeito</button>
+    <div id="efeito" :class="aplicarClasse"></div>
+    <br>
+    <input type="text" v-model="classse">
+    <div :class="classse"></div>
+    <br>
+    <div :class="[{ x }, 'destaque']">Estou sem classe CSS :(</div>
+    <br>
+    <br>
+    <input type="text" v-model="classe1">
+    <input type="text" @input="setStyle">
+    <div :class="[classe1, { demo }]"></div>
+    <br>
+    <br>
+    <input type="text" v-model="cor1">
+    <div :style="[estilo1, { backgroundColor: cor1 }]"> </div>
+    <br>
+    <br>
+    <button @click="iniciarProgresso">Iniciar</button>
+    <div class="barra-progresso">
+      <div class="progresso" :style="{ width }"></div>
+    </div>
   </div>
 </template>
 <!--================ END TEMPLATE ================-->
@@ -21,9 +44,23 @@
 <!--================ SCRIPT ================-->
 <script setup>
 import { ref, computed } from "vue";
+
 const cor = ref("red");
 const largura = ref(100);
 const altura = ref(40);
+const aplicarClasse = ref("destaque");
+const x = ref(true);
+const demo = ref("demo")
+const classse = ref("");
+const classe1 = ref("");
+const cor1 = ref("");
+const estilo1 = ref({
+  width: '100px',
+  height: '100px',
+})
+const width = ref(0);
+
+
 const meuEstilo = computed(() => {
   return {
     "background-color": cor.value,
@@ -31,8 +68,31 @@ const meuEstilo = computed(() => {
     height: altura.value + 'px'
   }
 })
-// const v = { "background-color": cor.value }
+const iniciarEfeito = () => {
 
+  setInterval(() => {
+
+    aplicarClasse.value = aplicarClasse.value == "destaque" ? "encolher" : "destaque";
+  }, 1000)
+}
+
+const setStyle = (event) => {
+  if (event.target.value == "true") {
+    demo.value = true;
+  } else if (event.target.value == "false") {
+    demo.value = false;
+  }
+}
+const iniciarProgresso = () => {
+
+  let valor = 0;
+  const temporizador = setInterval(() => {
+    valor += 5;
+    width.value = `${valor}%`;
+    if (valor == 100) clearInterval(temporizador);
+  }, 500)
+
+}
 </script>
 
 
@@ -56,10 +116,11 @@ input {
 }
 
 .demo {
+  margin-top: 2rem;
   margin-left: 1.75rem;
   width: 100px;
   height: 100px;
-  background-color: rgb(59, 59, 59);
+  background-color: rgb(233, 127, 127);
 }
 
 .demo2 {
@@ -90,5 +151,45 @@ input {
 
 .girar {
   transform: rotate(45deg);
+}
+
+#efeito {
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+}
+
+.destaque {
+  background-color: red;
+  width: 200px !important;
+}
+
+.encolher {
+  border: 1px solid black;
+  width: 50px;
+  height: 50px;
+}
+
+.x {
+  background-color: aqua;
+  width: 400px;
+  height: 100px;
+}
+
+.y {
+  background-color: darkblue;
+  width: 100px;
+  height: 400px;
+}
+
+.barra-progresso {
+  height: 30px;
+  width: 500px;
+  border: 1px solid black;
+}
+
+.progresso {
+  background-color: red;
+  height: 100%;
 }
 </style>
