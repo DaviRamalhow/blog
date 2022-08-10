68c0f1e5968d558414c8435bc8d89835d22452f2
DaviRamalhow <daviramalho3011@gmail.com>
2022-08-10 13:24:58 -0300

    v-for

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
 
   <div id="app">
-
-    <div v-if="logado">
-      <p>Usuario Logado: {{ nome }}</p>
-      <p>Perfil: Admin</p>
-    </div>
-
-    <p v-else-if="anonimo">Navegando anonimamente</p>
-    <p v-else>Nennhum usaurio logado</p>
-
-    <button @click="logado = !logado">{{ logado ? "Sair" : "Entrar" }}</button>
-    <input type="checkbox" v-model="anonimo">
+    <ul>
+      <li v-for="(cor, i) in cores" :key="cor">{{ i + 1 }}) {{ cor }}</li>
+    </ul>
+    <ul>
+      <button @click="cores.push('branca')">Adicionar</button>
+      <li v-for="pessoa in pessoas" :key="pessoa">
+        <div v-for="(valor, chave, index) in pessoa" :key="valor">
+          {{ chave }} = {{ valor }} ({{ index + 1 }})
+        </div>
+      </li>
+    </ul>
+    <hr>
+    <span v-for="n in 10" :key="n">{{ n }}</span>
   </div>
 </template>
 <!--================ END TEMPLATE ================-->
@@ -21,10 +23,19 @@
 <!--================ SCRIPT ================-->
 <script setup>
 import { ref } from "vue";
-const nome = ref("Maria");
-const logado = ref(false);
-const anonimo = ref(false);
+const cores = ref([
+  'vermelho',
+  'verde',
+  'amarelo',
+  'azul',
+  "roxo",
+  "aqua"
+])
 
+const pessoas = ref([
+  { nome: "Ana", idade: 26, peso: 59 },
+  { nome: "Guilherme", idade: 22, peso: 89 }
+])
 </script>
 
 
@@ -34,5 +45,9 @@ const anonimo = ref(false);
 #app {
   font-family: 'Courier New', Courier, monospace;
   display: flex;
+}
+
+ul {
+  list-style: none;
 }
 </style>
